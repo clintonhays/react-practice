e6ba5cea17510c53a6a51141642b30ca6dcd4ac7
clintonhays <clintonhays84@gmail.com>
2020-11-08 11:17:02 +0900

    add initial files and first component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  const title = "Tasha's Cauldron of Everything";
+  const author = 'Wizards RPG Team';
+  return (
+    <article className="book">
+      <img src="https://images-na.ssl-images-amazon.com/images/I/81Me-r4UL1L._AC_UL200_SR200,200_.jpg" alt="DnD Book" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{}</p>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
